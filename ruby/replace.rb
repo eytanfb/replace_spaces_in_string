688225b8a07d0eefc4adb0185714860a1545dbc1
Eytan Anjel <anjel@usc.edu>
2013-12-24 22:24:52 +0200

    tests added

--- ruby/replace.rb
+++ ruby/replace.rb
@@ -1,2 +1,15 @@
 module Replace
+  
+  def self.spaces_in_string(string)
+    raise NotEnoughSpacesError if !string || string.length < 3
+    number_of_words = string.split.size
+    string.strip
+  end
+  
+  def self.get_number_of_words_in(string)
+    string.split.size
+  end
+  
 end
+
+class NotEnoughSpacesError < StandardError; end
---
+++ ruby/tc_replace.rb
@@ -0,0 +1,66 @@
+require "test/unit"
+
+require_relative "replace"
+
+class TestReplace < Test::Unit::TestCase
+
+  def test_string_with_not_enough_space_should_raise_not_enough_error_expection
+    string = nil
+    assert_raise( NotEnoughSpacesError ) { Replace.spaces_in_string string }
+    string = ""
+    assert_raise( NotEnoughSpacesError ) { Replace.spaces_in_string string }
+    string = " "
+    assert_raise( NotEnoughSpacesError ) { Replace.spaces_in_string string }
+    string = "  "
+    assert_raise( NotEnoughSpacesError ) { Replace.spaces_in_string string }
+  end
+  
+  def test_empty_string_with_enough_space_should_return_empty_string_with_length_0
+    empty_string = "   "
+    expected = ""
+    actual = Replace.spaces_in_string empty_string
+    assert_equal expected, actual
+  end
+  
+  def test_get_number_of_words_in_string_should_be_0_if_string_is_empty
+    empty_string = "   "
+    expected = 0
+    actual = Replace.get_number_of_words_in empty_string
+    assert_equal expected, actual
+  end
+  
+  def test_get_number_of_words_in_string
+    string = "a   "
+    expected = 1
+    actual = Replace.get_number_of_words_in string
+    assert_equal expected, actual
+    string = "string with spaces"
+    expected = 3
+    actual = Replace.get_number_of_words_in string
+    assert_equal expected, actual
+  end
+  
+  def test_string_with_only_spaces_at_the_end_should_return_the_content_without_replacing_the_spaces
+   string = "a   "
+   expected = "a"
+   actual = Replace.spaces_in_string string
+   assert_equal expected, actual
+   string = "string           "
+   expected = "string"
+   actual = Replace.spaces_in_string string
+   assert_equal expected, actual
+  end
+  
+  def test_string_with_spaces_only_on_both_sides
+    string = "  string  "
+    expected = "string"
+    actual = Replace.spaces_in_string string
+    assert_equal expected, actual
+  end
+  
+  def test_string_with_spaces_in_the_middle_should_be_replaced_if_there_are_enough_space
+    string = "string with spaces"
+    assert_raise( NotEnoughSpacesError ) { Replace.spaces_in_string string }
+  end
+  
+end
